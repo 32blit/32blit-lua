a30316bd9246215bb01e2002f0f3a341ccb2c9dd
Phil Howard <phil@gadgetoid.com>
2021-01-06 15:03:18 +0000

    Implement Pen and Point

--- luablitlib.cpp
+++ luablitlib.cpp
@@ -5,15 +5,86 @@
 
 #include "luablitlib.hpp"
 
+#define LUA_BLIT_POINT "Point"
+#define LUA_BLIT_PEN "Pen"
+
 using namespace blit;
+
+static int pen_new(lua_State* L){
+    int32_t a = 255;
+    int nargs = lua_gettop(L);
+    int32_t r = (int32_t)luaL_checknumber(L, 1);
+    int32_t g = (int32_t)luaL_checknumber(L, 2);
+    int32_t b = (int32_t)luaL_checknumber(L, 3);
+    if(nargs == 4){
+        a = (int32_t)luaL_checknumber(L, r);
+    }
+    lua_pop(L, nargs);
+    *reinterpret_cast<Pen**>(lua_newuserdata(L, sizeof(Pen*))) = new Pen(r, g, b, a);
+    luaL_setmetatable(L, LUA_BLIT_PEN);
+    return 1;
+}
+
+static int pen_delete(lua_State* L){
+    delete *reinterpret_cast<Pen**>(lua_touserdata(L, 1));
+    return 0;
+}
+
+static int pen_index(lua_State* L){
+    int nargs = lua_gettop(L);
+    Pen pen = **reinterpret_cast<Pen**>(lua_touserdata(L, 1));
+    std::string method = luaL_checkstring(L, 2);
+    lua_pop(L, nargs);
+    nargs = 0;
+    if(method == "r") {lua_pushnumber(L, pen.r); nargs=1;}
+    if(method == "g") {lua_pushnumber(L, pen.g); nargs=1;}
+    if(method == "b") {lua_pushnumber(L, pen.b); nargs=1;}
+    if(method == "a") {lua_pushnumber(L, pen.a); nargs=1;}
+    return nargs;
+}
+
+Pen lua_blit_checkpen(lua_State *L, int arg) {
+    auto pen = luaL_checkudata(L, arg, LUA_BLIT_PEN);
+    return **reinterpret_cast<Pen**>(pen);
+}
+
+static int point_new(lua_State* L){
+    int nargs = lua_gettop(L);
+    int32_t x = (int32_t)luaL_checknumber(L, 1);
+    int32_t y = (int32_t)luaL_checknumber(L, 2);
+    lua_pop(L, nargs);
+    *reinterpret_cast<Point**>(lua_newuserdata(L, sizeof(Point*))) = new Point(x, y);
+    luaL_setmetatable(L, LUA_BLIT_POINT);
+    return 1;
+}
+
+static int point_delete(lua_State* L){
+    delete *reinterpret_cast<Point**>(lua_touserdata(L, 1));
+    return 0;
+}
+
+static int point_index(lua_State* L){
+    int nargs = lua_gettop(L);
+    Point point = **reinterpret_cast<Point**>(lua_touserdata(L, 1));
+    std::string method = luaL_checkstring(L, 2);
+    lua_pop(L, nargs);
+    nargs = 0;
+    if(method == "x") {lua_pushnumber(L, point.x); nargs=1;}
+    if(method == "y") {lua_pushnumber(L, point.y); nargs=1;}
+    return nargs;
+}
+
+Point lua_blit_checkpoint(lua_State *L, int arg) {
+    auto point = luaL_checkudata(L, arg, LUA_BLIT_POINT);
+    return **reinterpret_cast<Point**>(point);
+}
 
 static int sprite(lua_State *L) {
     int nargs = lua_gettop(L);
     unsigned int i = luaL_checknumber(L, 1);
-    unsigned int x = luaL_checknumber(L, 2);
-    unsigned int y = luaL_checknumber(L, 3);
+    Point p = lua_blit_checkpoint(L, 2);
     lua_pop(L, nargs);
-    screen.sprite(i, Point(x, y));
+    screen.sprite(i, p);
     return 0;
 }
 
@@ -31,15 +102,18 @@ static int load_sprites(lua_State *L) {
     return 0;
 }
 
+static int pen(lua_State *L) {
+    int nargs = lua_gettop(L);
+    Pen p = lua_blit_checkpen(L, 1);
+    lua_pop(L, nargs);
+    screen.pen = p;
+    return 0;
+}
+
 static int pixel(lua_State *L) {
     int nargs = lua_gettop(L);
-    unsigned int x = luaL_checknumber(L, 1);
-    unsigned int y = luaL_checknumber(L, 2);
-    unsigned int r = luaL_checknumber(L, 3);
-    unsigned int g = luaL_checknumber(L, 4);
-    unsigned int b = luaL_checknumber(L, 5);
-    screen.pen = Pen(r, g, b, 255);
-    screen.pixel(Point(x, y));
+    Point p = lua_blit_checkpoint(L, 1);
+    screen.pixel(p);
     lua_pop(L, nargs);
     return 0;
 }
@@ -59,14 +133,30 @@ static int debug(lua_State* L) {
 }
 
 static const luaL_Reg funcs[] = {
-  {"pixel", pixel},
-  {"debug", debug},
-  {"load_sprites", load_sprites},
-  {"sprite", sprite},
-  {NULL, NULL}
+    {"pen", pen},
+    {"pixel", pixel},
+    {"debug", debug},
+    {"load_sprites", load_sprites},
+    {"sprite", sprite},
+    {NULL, NULL}
 };
 
 LUAMOD_API int luaopen_blit (lua_State *L) {
-  luaL_newlib(L, funcs);
-  return 1;
+    luaL_newlib(L, funcs);
+
+    // Point
+    lua_register(L, LUA_BLIT_POINT, point_new);
+    luaL_newmetatable(L, LUA_BLIT_POINT);
+    lua_pushcfunction(L, point_delete); lua_setfield(L, -2, "__gc");
+    lua_pushcfunction(L, point_index); lua_setfield(L, -2, "__index");
+    lua_pop(L, 1);
+
+    // Pen
+    lua_register(L, LUA_BLIT_PEN, pen_new);
+    luaL_newmetatable(L, LUA_BLIT_PEN);
+    lua_pushcfunction(L, pen_delete); lua_setfield(L, -2, "__gc");
+    lua_pushcfunction(L, pen_index); lua_setfield(L, -2, "__index");
+    lua_pop(L, 1);
+
+    return 1;
 }
--- main.lua
+++ main.lua
@@ -1,20 +1,27 @@
 function init()
     blit.debug("hello world")
     blit.load_sprites("dingbads.bin")
+
+    local point = Point(10, 10);
+    local point1 = Point(point.x, point.y)
+    local test = Pen(255, 0, 255)
+
+    blit.pen(255, 255, 255)
 end
     
 function update(time)
 end
     
 function render(time)
-    blit.pixel(10, 10, 0, 255, 0)
-    blit.pixel(11, 10, 0, 255, 0)
-    blit.pixel(10, 11, 0, 255, 0)
-    blit.pixel(11, 11, 0, 255, 0)
+    blit.pen(Pen(255, 0, 255))
+    blit.pixel(Point(10, 10))
+    blit.pixel(Point(11, 10))
+    blit.pixel(Point(10, 11))
+    blit.pixel(Point(11, 11))
     local x = math.random(0, 319)
     local y = math.random(0, 239)
-    blit.pixel(x, y, 0, 255, 0)
+    blit.pixel(Point(x, y))
     local i = math.random(0, 16)
-    blit.sprite(i, x, y)
+    blit.sprite(i, Point(x, y))
     --blit.debug(time)
 end
